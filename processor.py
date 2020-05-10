2f67ec4101e04dd30909836873244833a10e437f
Aleksandr Neumoin <63591184+skripnn@users.noreply.github.com>
2020-05-10 12:44:32 +0300

    Add files via upload
    Numeric Matrix Processor - Stage 4 Completed

--- processor.py
+++ processor.py
@@ -1,7 +1,4 @@
 class Matrix:
-    matrix = None
-    n = None
-    m = None
 
     def __init__(self, n, m):  # Creating a Matrix with N rows and M columns
         self.n = int(n)
@@ -10,7 +7,7 @@ class Matrix:
         for _ in range(self.n):
             self.input_row(input())
 
-    def input_row(self, row):
+    def input_row(self, row):   # Input one row
         row = [float(n) if '.' in n else int(n) for n in row.split()]
         self.matrix.append(row)
 
@@ -19,17 +16,37 @@ class Menu:
     text = '''1. Add matrices
 2. Multiply matrix by a constant
 3. Multiply matrices
+4. Transpose matrix
 0. Exit'''
 
-    def start(self, n):
-        if n == 1:
-            self._1()
-        elif n == 2:
-            self._2()
-        elif n == 3:
-            self._3()
-        elif n == 0:
-            self._0()
+    text_4 = '''
+1. Main diagonal
+2. Side diagonal
+3. Vertical line
+4. Horizontal line'''
+
+    def __init__(self):
+        print(self.text)
+        self.state = [0]
+
+    def start(self, n):         # Menu algorithm
+        if len(self.state) == 1:
+            if n == 1:
+                self._1()
+            elif n == 2:
+                self._2()
+            elif n == 3:
+                self._3()
+            elif n == 4:
+                print(self.text_4)
+                self.state.append(4)
+            elif n == 0:
+                self._0()
+        elif len(self.state) == 2:
+            if self.state[1] == 4:
+                self._4(n)
+            self.state.pop()
+            print(self.text)
 
     def _1(self):
         x, y = map(int, input('Enter size of first matrix: ').split())
@@ -62,6 +79,19 @@ class Menu:
         del matrix_a
         del matrix_b
 
+    def _4(self, n):
+        x, y = map(int, input('Enter matrix size: ').split())
+        print('Enter matrix: ')
+        matrix_a = Matrix(x, y)
+        if n == 1:
+            transposition_main_diagonal(matrix_a)
+        elif n == 2:
+            transposition_side_diagonal(matrix_a)
+        elif n == 3:
+            transposition_by_vertical(matrix_a)
+        elif n == 4:
+            transposition_by_horizontal(matrix_a)
+
     def _0(self):
         exit()
 
@@ -75,6 +105,7 @@ def print_result(matrix):
     for n in matrix:
         n = map(str, n)
         print(' '.join(n))
+    print('')
 
 
 def adding(a, b):
@@ -113,8 +144,50 @@ def multiplication(a, b):
     print_result(matrix)
 
 
+def transposition_main_diagonal(a):
+    matrix = []
+    for n in range(a.n):
+        matrix.append([])
+        for m in range(a.m):
+            x = m
+            y = n
+            matrix[n].append(a.matrix[x][y])
+    print_result(matrix)
+
+
+def transposition_side_diagonal(a):
+    matrix = []
+    for n in range(a.m):
+        matrix.append([])
+        for m in range(a.n):
+            x = (a.n - 1) - m
+            y = (a.m - 1) - n
+            matrix[n].append(a.matrix[x][y])
+    print_result(matrix)
+
+
+def transposition_by_vertical(a):
+    matrix = []
+    for n in range(a.n):
+        matrix.append([])
+        for m in range(a.m):
+            x = n
+            y = (a.m - 1) - m
+            matrix[n].append(a.matrix[x][y])
+    print_result(matrix)
+
+
+def transposition_by_horizontal(a):
+    matrix = []
+    for n in range(a.n):
+        matrix.append([])
+        for m in range(a.m):
+            x = (a.n - 1) - n
+            y = m
+            matrix[n].append(a.matrix[x][y])
+    print_result(matrix)
+
+
 menu = Menu()
 while True:
-    print(menu.text)
     menu.start(int(input('Your choice: ')))
-    print('')
