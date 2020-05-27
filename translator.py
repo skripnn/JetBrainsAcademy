96523579b4e6bc7ecbcffa2d2a0286990a1bae3f
Aleksandr Neumoin <63591184+skripnn@users.noreply.github.com>
2020-05-27 19:36:00 +0300

    Add files via upload
    Multilingual Online Translator 4/7

--- translator.py
+++ translator.py
@@ -1,28 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
-import lxml
+
+languages = ['Arabic',
+             'German',
+             'English',
+             'Spanish',
+             'French',
+             'Hebrew',
+             'Japanese',
+             'Dutch',
+             'Polish',
+             'Portuguese',
+             'Romanian',
+             'Russian',
+             'Turkish'
+             ]
 
 
 def start():
-    language, word = input_params()
-    url = get_url(language, word)
+    lang_from, lang_to, word = choose_languages()
+    url = get_url(lang_from, lang_to, word)
     html = get_response(url)
+    if html is None:
+        exit()
     words, phrases = parser(html)
-    print_examples(words, phrases, language)
+    print_examples(words, phrases, lang_to)
+
+
+def choose_languages():
+    print("Hello, you're welcome to the translator. Translator supports:")
+    for n, language in enumerate(languages):
+        print(f'{n + 1}. {language}')
+    lang_from = input('Type the number of your language:\n')
+    lang_from = languages[int(lang_from) - 1]
+    lang_to = input('Type the number of language you want to translate to:\n')
+    lang_to = languages[int(lang_to) - 1]
+    word = input('Type the word you want to translate:\n')
+    return lang_from, lang_to, word
 
 
 def print_examples(words, phrases, language):
-    if language == 'fr':
-        lang = 'French'
-    elif language == 'en':
-        lang = 'English'
     print('\nContext examples:')
-    print(f'\n{lang} Translations:')
+    print(f'\n{language} Translations:')
     for n, i in enumerate(words):
         print(i)
         if n == 4:
             break
-    print(f'\n{lang} Examples:')
+    print(f'\n{language} Examples:')
     for n, i in enumerate(phrases):
         if n % 2 == 0:
             print(f'{i}:')
@@ -33,14 +57,13 @@ def print_examples(words, phrases, language):
             print('')
 
 
-
-def input_params():
-    language = input(
-        'Type "en" if you want to translate from French into English, '
-        'or "fr" if you want to translate from English into French:\n')
-    word = input('Type the word you want to translate:\n')
-    print(f'You chose "{language}" as the language to translate "{word}" to.')
-    return language, word
+# def input_params():
+#     language = input(
+#         'Type "en" if you want to translate from French into English, '
+#         'or "fr" if you want to translate from English into French:\n')
+#     word = input('Type the word you want to translate:\n')
+#     print(f'You chose "{language}" as the language to translate "{word}" to.')
+#     return language, word
 
 
 def get_response(url):
@@ -48,19 +71,14 @@ def get_response(url):
     response = requests.get(url, headers={'User-Agent': user_agent})
     if response.status_code == 200:
         print(response.status_code, 'OK')
+        return response.text
     else:
         print(response.status_code)
-    return response.text
-
-
-def get_url(language, word):
-    if language == 'fr':
-        lang = 'english-french'
-    elif language == 'en':
-        lang = 'french-english'
-    else:
         return None
-    url = f"https://context.reverso.net/translation/{lang}/{word}"
+
+
+def get_url(lang_from, lang_to, word):
+    url = f'https://context.reverso.net/translation/{lang_from.lower()}-{lang_to.lower()}/{word}'
     return url
 
 
@@ -70,10 +88,6 @@ def parser(html):
     words_list = get_list_from_soup(soup, words_tags)
     phrases_tags = ['div.src.ltr', 'div.trg.ltr']
     phrases_list = get_list_from_soup(soup, phrases_tags)
-    # words = ', '.join(words_list)
-    # phrases = ', '.join(phrases_list)
-    # print('Translation, ' + words)
-    # print('Translation, ' + phrases)
     return words_list, phrases_list
 
 
@@ -85,8 +99,6 @@ def get_list_from_soup(soup, tags):
         phrase = soup_.get_text()
         result_list.append(phrase.strip())
     return result_list
-
-
 
 
 start()
