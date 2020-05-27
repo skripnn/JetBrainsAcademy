b1d49e84b91c4fad27ad2ec50e0efd9495c850d7
Aleksandr Neumoin <63591184+skripnn@users.noreply.github.com>
2020-05-27 21:00:54 +0300

    Add files via upload
    Multilingual Online Translator 5/7

--- translator.py
+++ translator.py
@@ -19,12 +19,22 @@ languages = ['Arabic',
 
 def start():
     lang_from, lang_to, word = choose_languages()
-    url = get_url(lang_from, lang_to, word)
-    html = get_response(url)
-    if html is None:
-        exit()
-    words, phrases = parser(html)
-    print_examples(words, phrases, lang_to)
+    if type(lang_to) is list:
+        file = f'{word}.txt'
+        with open(file, 'w') as file:
+            for language in lang_to:
+                url = get_url(lang_from, language, word)
+                html = get_response(url)
+                words, phrases = parser(html)
+                examples = make_examples(words, phrases, language, 1)
+                print(examples)
+                file.write(examples + '\n')
+
+    else:
+        url = get_url(lang_from, lang_to, word)
+        html = get_response(url)
+        words, phrases = parser(html)
+        print_examples(words, phrases, lang_to, 5)
 
 
 def choose_languages():
@@ -33,47 +43,45 @@ def choose_languages():
         print(f'{n + 1}. {language}')
     lang_from = input('Type the number of your language:\n')
     lang_from = languages[int(lang_from) - 1]
-    lang_to = input('Type the number of language you want to translate to:\n')
-    lang_to = languages[int(lang_to) - 1]
-    word = input('Type the word you want to translate:\n')
+    lang_to = input("Type the number of language you want to translate to or '0' to translate to all languages:\n")
+    if lang_to != '0':
+        lang_to = languages[int(lang_to) - 1]
+    else:
+        lang_to = languages
+        lang_to.remove(lang_from)
+    word = input('Type the word you want to translate:\n').lower()
     return lang_from, lang_to, word
 
 
-def print_examples(words, phrases, language):
-    print('\nContext examples:')
-    print(f'\n{language} Translations:')
+def make_examples(words, phrases, language, examples_number):
+    examples = ''
+    examples += f'{language} Translations:' + '\n'
     for n, i in enumerate(words):
-        print(i)
-        if n == 4:
+        examples += i + '\n'
+        if n == examples_number - 1:
             break
-    print(f'\n{language} Examples:')
+    examples += f'\n{language} Examples:' + '\n'
     for n, i in enumerate(phrases):
         if n % 2 == 0:
-            print(f'{i}:')
+            examples += f'{i}:' + '\n'
         else:
-            print(f'{i}')
-            if n // 2 == 4:
+            examples += f'{i}' + '\n\n'
+            if n // 2 == examples_number - 1:
                 break
-            print('')
+    return examples
 
 
-# def input_params():
-#     language = input(
-#         'Type "en" if you want to translate from French into English, '
-#         'or "fr" if you want to translate from English into French:\n')
-#     word = input('Type the word you want to translate:\n')
-#     print(f'You chose "{language}" as the language to translate "{word}" to.')
-#     return language, word
+def print_examples(words, phrases, language, examples_number):
+    print('\nContext examples:\n')
+    print(make_examples(words, phrases, language, examples_number))
 
 
 def get_response(url):
     user_agent = 'Mozilla/5.0'
     response = requests.get(url, headers={'User-Agent': user_agent})
     if response.status_code == 200:
-        print(response.status_code, 'OK')
         return response.text
     else:
-        print(response.status_code)
         return None
 
 
@@ -84,10 +92,18 @@ def get_url(lang_from, lang_to, word):
 
 def parser(html):
     soup = BeautifulSoup(html, features="lxml")
-    words_tags = ['a.translation.ltr.dict.first.n', 'a.translation.ltr.dict.no-pos']
-    words_list = get_list_from_soup(soup, words_tags)
-    phrases_tags = ['div.src.ltr', 'div.trg.ltr']
-    phrases_list = get_list_from_soup(soup, phrases_tags)
+    words_tags = ['a.translation.ltr.dict.first.n',
+                  'a.translation.ltr.dict.no-pos',
+                  'a.translation.rtl.dict.first.n',
+                  'a.translation.rtl.dict.no-pos']
+    words_part = soup.body.find(id='top-results')
+    words_list = get_list_from_soup(words_part, words_tags)
+    phrases_tags = ['div.src.ltr',
+                    'div.trg.ltr',
+                    'div.src.rtl',
+                    'div.trg.rtl']
+    phrases_part = soup.body.find(id='examples-content')
+    phrases_list = get_list_from_soup(phrases_part, phrases_tags)
     return words_list, phrases_list
 
 
