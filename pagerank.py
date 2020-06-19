1c98bf7c4815ea251981bbc04f81da22d4471347
Aleksandr Neumoin <63591184+skripnn@users.noreply.github.com>
2020-06-19 13:09:45 +0300

    Add files via upload
    PageRank 2/5

--- pagerank.py
+++ pagerank.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.linalg as la
 from io import StringIO
+import numpy.linalg as la
 
 L = np.array([
     [0, 1 / 2, 1 / 3, 0, 0, 0],
@@ -11,14 +12,43 @@ L = np.array([
     [0, 0, 1 / 3, 0, 0, 0]
 ])
 
-s = StringIO()
-np.savetxt(s, L, fmt="%.3f")
-print(s.getvalue(), end='')
+# L = np.array([
+#     [0, 1 / 2, 1 / 3, 0, 0, 0],
+#     [1 / 3, 0, 0, 0, 1 / 2, 1 / 3],
+#     [1 / 3, 1 / 2, 0, 1, 0, 0],
+#     [1 / 3, 0, 1 / 3, 0, 1 / 2, 1 / 3],
+#     [0, 0, 0, 0, 0, 1 / 3],
+#     [0, 0, 1 / 3, 0, 0, 0]
+# ])
 
-e_vals, e_vecs = la.eig(L)
-vec = np.transpose(e_vecs)[0]
 
-for i in vec:
-    x = i * 100 / sum(vec)
-    x = round(x.real, 3)
-    print(x)
+def get_value_for_print(matrix):
+    s = StringIO()
+    np.savetxt(s, matrix, fmt="%.3f")
+    return s.getvalue()
+
+
+# e_vals, e_vecs = la.eig(L)
+# vec = np.transpose(e_vecs)[0]
+#
+# for i in vec:
+#     x = i * 100 / sum(vec)
+#     x = round(x.real, 3)
+#     print(x)
+
+r = 100 * np.ones(6) / 6
+r = L @ r
+print(get_value_for_print(r))
+
+for _ in range(10):
+    r = L @ r
+print(get_value_for_print(r))
+
+r_prev = r
+r_next = L @ r
+while la.norm(r_prev - r_next) > 0.001:
+    r = r_next
+    r_prev = r
+    r_next = L @ r
+print(get_value_for_print(r))
+
